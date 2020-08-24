8dd0b4e514bbaedb8ada2f015454ed922695c277
Sean Park <sunjun1225@gmail.com>
2020-08-24 19:30:40 +0900

    modify script not to check yes command from user

--- setting.sh
+++ setting.sh
@@ -1,17 +1,22 @@
 apt-get install sudo
 sudo apt-get update -y
-sudo apt-get install wget curl vim 
+sudo apt-get install -y wget curl vim 
 
 # install silver-searcher / zsh / git / tmux
-sudo apt-get install silversearcher-ag zsh git tmux tree -y
+sudo apt-get install -y silversearcher-ag zsh git tmux tree
 
+# clone sean's dev enviroment
 git clone https://github.com/Joon-Park92/custom_dotfiles.git ~/custom_dotfiles
 
 # Vundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # YCM
-sudo apt-get install build-essential cmake python3-dev -y
+sudo apt-get install -y build-essential cmake python3-dev
+
+# install dependencies for python-build
+sudo apt-get install -y build-essential libreadline-dev zlib1g-dev libbz2-dev libsqlite3-dev libssl-dev
+
 git clone https://github.com/ycm-core/YouCompleteMe.git ~/.vim/bundle
 
 # vim setting
@@ -27,7 +32,7 @@ cp ~/custom_dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/.vim/bundle/vim-flake8/ftplugin ~/.vim/bundle/vim-flake8/plugin
 
 # ycm for javascript / nodejs to node remapping
-sudo apt-get install nodejs npm
+sudo apt-get install -y nodejs npm
 sudo ln -s /usr/bin/nodejs /usr/bin/node
 
 # install js-beautify
@@ -57,9 +62,6 @@ eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"                                       
 
 EOL
-
-# install dependencies for python-build
-sudo apt-get install build-essential libreadline-dev zlib1g-dev libbz2-dev libsqlite3-dev libssl-dev
 
 # install python
 source ~/.zshrc
